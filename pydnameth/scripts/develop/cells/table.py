0af6583c60a9fa78ac4ec7cfc80389cacd9edc26
Archie Hewitt <hewitt.archie@yandex.ru>
2019-06-19 14:15:09 +0300

    cells ancova

--- pydnameth/scripts/develop/cells/table.py
+++ pydnameth/scripts/develop/cells/table.py
@@ -1,4 +1,4 @@
-from pydnameth.scripts.develop.table import table_z_test_linreg
+from pydnameth.scripts.develop.table import table_z_test_linreg, table_ancova
 from pydnameth.config.experiment.types import DataType
 
 
@@ -9,6 +9,24 @@ def cells_table_z_test_linreg(
     observables_list
 ):
     table_z_test_linreg(
+        data_type=DataType.cells,
+        data=data,
+        annotations=annotations,
+        attributes=attributes,
+        observables_list=observables_list,
+        data_params=None,
+        task_params=None,
+        method_params=None
+    )
+
+
+def cells_table_ancova(
+    data,
+    annotations,
+    attributes,
+    observables_list
+):
+    table_ancova(
         data_type=DataType.cells,
         data=data,
         annotations=annotations,
