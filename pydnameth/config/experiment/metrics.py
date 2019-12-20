82c33933a27770c6a1c70a74e72effb73df80b00
Unknown <hewitt.archie@yandex.ru>
2019-12-20 20:12:50 +0300

    fixed metrics bug

--- pydnameth/config/experiment/metrics.py
+++ pydnameth/config/experiment/metrics.py
@@ -19,7 +19,7 @@ def get_method_metrics_keys(config):
                 metrics = [
                     'item',
                     'aux',
-                    'type'
+                    'type',
                     'bp_lm',
                     'bp_lm_pvalue',
                     'bp_lm_pvalue_fdr_bh',
